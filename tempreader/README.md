ca5fe909815038b523bb2cd6aea7689ead80c907
Bertrik Sikken <bertrik@sikken.nl>
2024-06-16 23:18:37 +0200

    Add 1-wire temperature reader / publisher script.

---
+++ tempreader/README.md
@@ -0,0 +1,5 @@
+Script to read temperature from a 1-wire sensor and publish it on MQTT for further processing.
+
+External dependencies:
+* paho-mqtt
+
---
+++ tempreader/tempreader.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import time
+import paho.mqtt.client as mqtt
+
+def read_temperature(sensor_id):
+    sensor_file = f"/mnt/1wire/uncached/{sensor_id}/temperature"
+    try:
+        with open(sensor_file, 'r', encoding="UTF-8") as file:
+            temperature = file.read().strip()
+            return temperature
+    except FileNotFoundError:
+        print(f"Sensor {sensor_id} not found.")
+        return None
+
+def main():
+    """ The main entry point """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--sensor", help="The temperature sensor id",
+                        default="28.7AAB46D42000")
+    parser.add_argument("-b", "--broker", help="The MQTT broker host name",
+                        default="stofradar.nl")
+    parser.add_argument("-t", "--topic", help="The MQTT base topic to publish on",
+                        default="karaburan")
+    parser.add_argument("-i", "--interval", help="The publish interval (seconds)", default=15)
+    args = parser.parse_args()
+
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.connect(args.broker)
+    while True:
+        # get value
+        timestamp = time.time()
+        value = read_temperature(args.sensor)
+
+        # build payload
+        payload = {}
+        payload['time'] = timestamp
+        payload['type'] = 'temperature'
+        payload['id'] = args.sensor
+        payload['value'] = value
+        topic = f"{args.topic}/sensors/temperature/{args.sensor}/measurement"
+
+        # publish
+        print(f"Sending {payload} to {topic}")
+        client.publish(topic, json.dumps(payload))
+
+        # wait
+        time.sleep(args.interval)
+
+if __name__ == "__main__":
+    main()
