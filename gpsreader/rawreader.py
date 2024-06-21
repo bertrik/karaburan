4832737074b8a0e10f734351d822e141fb14c053
Bertrik Sikken <bertrik@gmail.com>
2024-06-21 20:07:41 +0100

    Update and simplify gps reader script

--- gpsreader/rawreader.py
+++ gpsreader/rawreader.py
@@ -2,6 +2,7 @@
 
 import argparse
 import socket
+import time
 import json
 import paho.mqtt.client as mqtt
 
@@ -9,14 +10,12 @@ import paho.mqtt.client as mqtt
 HOST = 'localhost'
 PORT = 2947
 
-class GpsdClient(object):
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
+class GpsdClient():
+    def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self):
-        self.sock.connect((self.host, self.port))
+    def connect(self, host, port):
+        self.sock.connect((host, port))
 
         # Send the command to gpsd to watch for data
         self.sock.sendall(b'?WATCH={"enable":true,"json":true}\n')
@@ -26,7 +25,7 @@ class GpsdClient(object):
         if not data:
             return data
         data = data.decode('utf-8').strip()
-        return data
+        return data.split('\n')
 
     def close(self):
         self.sock.close()
@@ -41,42 +40,36 @@ def main():
     # set up topic structure
     base_topic = f"karaburan/sensors/gps/{args.sensor}"
     config_topic = f"{base_topic}/config"
-    config = {'name': 'location', 'unit': 'lat/lon/alt', 'location': 'topside', 'contact': 'bertrik'}
+    config = {'name': 'location', 'unit': 'NA', 'location': 'topside', 'contact': 'bertrik'}
 
     # connect to mqtt
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     client.will_set(config_topic, payload=None, retain=True)
     client.connect(args.broker)
-    client.publish(config_topic, json.dumps(config), retain=True)
 
     # connect to gpsd
-    gpsd_client = GpsdClient(HOST, PORT)
-    gpsd_client.connect()
-
+    gpsd_client = GpsdClient()
+    gpsd_client.connect(HOST, PORT)
     try:
         while True:
-            data = gpsd_client.poll()
+            reports = gpsd_client.poll()
 
             # Decode and parse the JSON data
-            reports = data.split('\n')
-            for report in reports:
-                if report:
-                    report = json.loads(report)
+            for r in reports:
+                report = json.loads(r)
+                print(f"{report}")
 
-                    # publish
+                # Check for gpsd connection to device, publish config to indicate our presence
+                if report['class'] == 'DEVICES':
+                    config['devices'] = report['devices']
+                    client.publish(config_topic, json.dumps(config), retain=True)
+
+                # Time position reports
+                if report['class'] == 'TPV':
                     topic = f"{base_topic}/measurement"
-                    client.publish(topic, json.dumps(report))
-
-                    # Check for TPV reports
-                    if report['class'] == 'TPV':
-                        latitude = report.get('lat', None)
-                        longitude = report.get('lon', None)
-                        altitude = report.get('alt', None)
-                        speed = report.get('speed', None)
-                        time = report.get('time', None)
-
-                        print(f"Latitude: {latitude}, Longitude: {longitude}, Altitude: {altitude}, Speed: {speed}, Time: {time}")
-
+                    timestamp = time.time()
+                    measurement = {'time': timestamp, 'type': 'gps', 'id': args.sensor, 'value': report}
+                    client.publish(topic, json.dumps(measurement))
     finally:
         gpsd_client.close()
 
