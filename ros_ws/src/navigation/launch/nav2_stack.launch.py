ef9d7691a8d50d8f25a9cce273a02eecc052c6ab
Michiel Leegwater <mleegwt@users.noreply.github.com>
2025-04-23 23:06:14 +0200

    Further fixes

--- ros_ws/src/navigation/launch/nav2_stack.launch.py
+++ ros_ws/src/navigation/launch/nav2_stack.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch_ros.actions import LifecycleNode
 import launch_testing.actions
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -11,34 +12,43 @@ def generate_launch_description():
         'config',
         'controller_server.yaml'
     )
+    behavior_yaml = os.path.join(
+        get_package_share_directory('navigation'),
+        'config',
+        'behavior_server.yaml'
+    )
     map_yaml = os.path.join(
         get_package_share_directory('navigation'),
         'maps',
         'empty.yml'
     )
     nodes = [
-        Node(
+        LifecycleNode(
             package='nav2_bt_navigator',
             executable='bt_navigator',
             name='bt_navigator',
+            namespace='',
             parameters=[{'use_sim_time': False}],
         ),
-        Node(
+        LifecycleNode(
             package='nav2_planner',
             executable='planner_server',
             name='planner_server',
+            namespace='',
             parameters=[{'use_sim_time': False}],
         ),
-        Node(
+        LifecycleNode(
             package='nav2_controller',
             executable='controller_server',
             name='controller_server',
+            namespace='',
             parameters=[controller_yaml],
         ),
-        Node(
+        LifecycleNode(
             package='nav2_map_server',
             executable='map_server',
             name='map_server',
+            namespace='',
             parameters=[{
                 'yaml_filename': map_yaml,
                 'use_sim_time': False
@@ -69,6 +79,30 @@ def generate_launch_description():
             name='static_tf_map_to_base_link',
             arguments=['0', '0', '0', '0', '0', '0', 'map', 'base_link'],
             output='screen'
+        ),
+        LifecycleNode(
+            package='nav2_behaviors',
+            executable='behavior_server',
+            name='behavior_server',
+            namespace='',
+            output='screen',
+            parameters=[behavior_yaml],
+        ),
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_navigation',
+            output='screen',
+            parameters=[{
+                'autostart': True,
+                'node_names': [
+                    'bt_navigator',
+                    'planner_server',
+                    'controller_server',
+                    'behavior_server',
+                    'map_server',
+                ]
+            }]
         ),
     ]
 
