48b3a5f4748347d37fe05dea0750d24da5b4a876
Michiel Leegwater <mleegwt@users.noreply.github.com>
2025-05-16 15:40:22 +0200

    Publishing simplified IMU data

--- ros_ws/src/sensorfusion/sensorfusion/sensorfusion_node.py
+++ ros_ws/src/sensorfusion/sensorfusion/sensorfusion_node.py
@@ -8,6 +8,7 @@ from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Quaternion
 from rclpy.node import Node
 from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import Imu
 from std_msgs.msg import Header
 from std_msgs.msg import Float64
 
@@ -29,37 +30,24 @@ class SensorFusionNode(Node):
 
     def __init__(self):
         super().__init__('sensor_fusion_node')
-        self.gps_sub = self.create_subscription(NavSatFix, '/fix', self.gps_callback, 10)
         self.compass_sub = self.create_subscription(Float64, '/compass', self.compass_callback, 10)
-        self.pose_pub = self.create_publisher(PoseWithCovarianceStamped, '/amcl_pose', 100)
-        self.current_lat = 0.0
-        self.current_lon = 0.0
+        self.imu_pub = self.create_publisher(Imu, '/imu/data', 100)
         self.current_heading = 0  # Heading from your custom compass
-
-    def gps_callback(self, msg):
-        self.current_lat = msg.latitude
-        self.current_lon = msg.longitude
-        self.publish_pose()
 
     def compass_callback(self, heading):
         self.current_heading = heading.data
         self.publish_pose()
 
     def publish_pose(self):
-        hdr = Header(stamp=self.get_clock().now().to_msg())
-
-        # Convert GPS to XY coordinates (this will need to be done with a transform library)
-        # Simplified; typically requires a transform
-        position = Point(x = self.current_lon, y = self.current_lat, z = 0.0)
+        hdr = Header(stamp=self.get_clock().now().to_msg(), frame_id='imu_link')
 
         # Convert compass heading to quaternion for orientation
         orientation = heading_to_quaternion(self.current_heading)
-        pose = Pose(position = position, orientation = orientation)
+        cov = [ 0.01, 0, 0, 0, 0.01, 0, 0, 0, 0.01]
+        imu = Imu(header = hdr, orientation = orientation, orientation_covariance = cov, angular_velocity_covariance = cov, linear_acceleration_covariance = cov)
 
-        pose_ext = PoseWithCovariance(pose = pose)
-        pose_msg = PoseWithCovarianceStamped(header = hdr, pose = pose_ext)
-        self.get_logger().debug(f"Pose with cov: {pose_msg}")
-        self.pose_pub.publish(pose_msg)
+        self.get_logger().debug(f"Pose with cov: {imu}")
+        self.imu_pub.publish(imu)
 
 def main():
     try:
