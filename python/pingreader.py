d30890916549f38eb0c1ba41f6e548e09bedc778
Bertrik Sikken <bertrik@sikken.nl>
2026-08-22 23:25:38 +0200

    Convert pingreader.py to use DataBusClient.

--- python/pingreader.py
+++ python/pingreader.py
@@ -3,17 +3,15 @@
 # Copyright 2024 Bertrik Sikken <bertrik@sikken.nl>
 
 import argparse
-import datetime
-import json
 import time
 from typing import Optional
 
-import paho.mqtt.client as mqtt
 import serial
+
+from databus import DataBusClient
 
 # Configure the serial port (make sure to replace 'COM3' with your actual serial port)
 BAUD_RATE = 9600  # Baud rate (can be adjusted based on your device)
-SENSOR_TYPE = 'distance'
 
 
 def ping(ser: object) -> Optional[int]:
@@ -33,35 +31,22 @@ def ping(ser: object) -> Optional[int]:
 def main():
     """ The main entry point """
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    DataBusClient.add_arguments(parser)
     parser.add_argument("-d", "--device", help="The serial device",
                         default="/dev/ttyUSB0")
-    parser.add_argument("-b", "--broker", help="The MQTT broker host name",
-                        default="localhost")
     parser.add_argument("-s", "--sensor", help="The sensor id",
                         default="pinger")
     parser.add_argument("-i", "--interval", help="The publish interval (seconds)", default=1)
     args = parser.parse_args()
 
-    # set up topic structure
-    base_topic = f"karaburan/sensors/{SENSOR_TYPE}/{args.sensor}"
-    config_topic = f"{base_topic}/config"
-    measurement_topic = f"{base_topic}/measurement"
-    config = {'type': SENSOR_TYPE, 'id': args.sensor, 'unit': 'mm', 'location': 'side'}
-
-    # connect to mqtt
-    client = mqtt.Client()
-    client.will_set(config_topic, payload=None, retain=True)
-    client.connect(args.broker)
-
     # Open the serial port
     with serial.Serial(args.device, BAUD_RATE, timeout=1) as ser:
-        client.publish(config_topic, json.dumps(config), retain=True)
+        client = DataBusClient(args.broker, "distance", args.id)
+        client.start()
         while True:
-            timestamp = datetime.datetime.now(datetime.UTC).isoformat()
             distance = ping(ser)
             if distance:
-                measurement = {'type': SENSOR_TYPE, 'id': args.sensor, 'time': timestamp, 'value': distance}
-                client.publish(measurement_topic, json.dumps(measurement))
+                client.publish(distance)
 
             # next
             time.sleep(args.interval)
